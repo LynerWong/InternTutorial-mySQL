e7798593b3f2b7a12f94e250b41abc93db2bb1bd
LynerWong <choonhong@gmail.com>
2018-10-12 11:31:49 +0800

    Update file

---
+++ src/main/java/respository/SimpleRespository.java
@@ -0,0 +1,10 @@
+package respository;
+
+import model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SimpleRespository extends JpaRepository<Person, String> {
+
+}
